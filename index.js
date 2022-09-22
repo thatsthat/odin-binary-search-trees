816d43b30c3831f2b517850fd7cea6895b6caa55
Aram Anto <aram.anto@gmail.com>
2022-09-22 15:30:47 +0200

    implemented 1, 2 & 3

--- index.js
+++ index.js
@@ -1,10 +1,57 @@
-const Node = (value = null, left = null, right = null) => {
-  return { value, left, right };
+const Node = (data = null, left = null, right = null) => {
+  return { data, left, right };
 };
 
-const iArr = [1, 2, 3, 4, 5, 6];
-let mid = (iArr.length - (iArr.length % 2)) / 2;
-const top = iArr[mid - 1];
-const left = iArr.slice(0, mid - 1);
-const right = iArr.slice(mid);
-console.log(iArr, top, left, right);
+const Tree = (inpArr) => {
+  let rootNode = null;
+
+  return {
+    rootNode,
+  };
+};
+
+function buildTree(inpArr) {
+  // Remove any duplicates
+  const noDups = [...new Set(inpArr)];
+  // Sort the array
+  const sortArr = noDups.sort((a, b) => a - b);
+
+  if (sortArr.length == 1) {
+    return Node(sortArr);
+  }
+
+  if (sortArr.length == 2) {
+    return Node(sortArr[0], null, Node(sortArr[1]));
+  }
+
+  const mid = (sortArr.length - (sortArr.length % 2)) / 2;
+  const top = sortArr[mid];
+  const left = sortArr.slice(0, mid);
+  const right = sortArr.slice(mid + 1);
+
+  return Node(top, buildTree(left), buildTree(right));
+}
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+const nArr = [1, 5, 4, 3, 8, 34, 5, 3, 18, 31, 13, 2];
+const tree = buildTree(nArr);
+console.log(JSON.stringify(tree));
+//console.log(prettyPrint(tree));
+//console.log([...new Set(nArr.sort((a, b) => a - b))]);
+
+/* const uArr = [...new Set(nArr)];
+const iArr = uArr.sort((a, b) => a - b);
+const mid = (iArr.length - (iArr.length % 2)) / 2;
+const top = iArr[mid];
+const left = iArr.slice(0, mid);
+const right = iArr.slice(mid + 1);
+console.log(left, top, right); */
