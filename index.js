97116041617c94beacf19f1cb6373be88873e4ef
Aram Anto <aram.anto@gmail.com>
2022-09-22 09:46:02 +0200

    calculate middle element of array

---
+++ index.js
@@ -0,0 +1,6 @@
+const iArr = [1, 2, 3, 4, 5, 6];
+let mid = (iArr.length - (iArr.length % 2)) / 2;
+const top = iArr[mid - 1];
+const left = iArr.slice(0, mid - 1);
+const right = iArr.slice(mid);
+console.log(iArr, top, left, right);
